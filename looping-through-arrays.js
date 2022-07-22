4c505ef472466d1acd20230d3c142a5086a04709
Nicholas Burgess <Nicholas.Burgess13@gmail.comgit config --global user.name nicholas>
2022-07-22 18:38:51 +0000

    initial commit with all of my work so far

---
+++ looping-through-arrays.js
@@ -0,0 +1,5 @@
+let pets =  ['cat', 'dog', 'rat'] 
+for (let pets [i] = pets [i] + 's'; ) {
+
+} 
+console.log(pets)
---
+++ rounding-numbers.js
@@ -0,0 +1,3 @@
+let roundUp = 1.5
+let test = Math.round(roundUp)
+console.log(test)
